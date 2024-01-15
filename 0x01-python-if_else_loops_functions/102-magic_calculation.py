21d888926702ce948ecacb453f2ba7f2f27c09f3
the-nyamira123 <nathanslay7@gmail.com>
2024-01-15 04:06:36 -0800

    bytecode

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
-# Author - NYAMIRA BABY 
+#!/usr/bin/env python3
 
 def magic_calculation(a, b, c):
-    """Match bytecode provided by Holberton School."""
     if a < b:
-        return (c)
-    if c > b:
-        return (a + b)
-    return (a*b - c)
+        result = c
+    elif c > b:
+        result = a + b
+    else:
+        result = a * b - c
+    return result
